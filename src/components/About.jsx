a4de526b83c5d5f058a39f5e24d28e88ed6bbe0e
Hassan Mushtaq <hhmushtaq@owu.edu>
2025-01-26 22:57:57 -0500

    used grid to create page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ const skills = [
 const About = () => {
   
   return (
-    <div id="about" className="relative h-dvh w-screen bg-primary flex flex-col overflow-x-hidden">
+    <div id="about" className="h-dvh relative w-screen bg-primary flex flex-col overflow-x-hidden">
         {/* top left text */}
         <h1 className="absolute top-[24px] left-[24px] font-black display text-secondary w-80 sm:w-fit">
           ABOUT ME
@@ -36,61 +36,66 @@ const About = () => {
                 alt="github" />
         </div>
 
-        {/* MAIN */}
-        <section className="main h-fit w-full pt-32 pe-28 p-[24px] flex flex-col lg:flex-row gap-24 justify-between">
+        {/* MAIN CONTAINER */}
+        <section id='main' className='h-fit md:h-full
+                                  flex flex-col
+                                  md:grid md:grid-rows-6 md:grid-cols-6 gap-8 pt-36 p-[24px] pe-28'>
           {/* img */}
-          <div className="w-full h-[35rem] lg:size-[35rem] border-[0.5px] border-secondary overflow-hidden">
+          <div className='border-[0.5px] border-secondary overflow-hidden
+                          min-w-[14rem] max-w-[24rem] min-h-[14rem] max-h-[30rem]
+                          md:col-span-2 md:row-span-6 md:h-full'>
+            <img></img>
           </div>
-          {/* right content */}
-          <div className="flex flex-col gap-24 md:gap-8 size-fit text-end">
-            {/* top stuff */}
-            <div>
-              <h3 className="sub-heading font-bold text-secondary mb-2">HELLO!</h3>
-              <p className="font-xs text-secondary font-thin">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, 
-                  ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>
-            </div>
-            {/* Skills */}
-            <div className="flex flex-col">
-              <h3 className="sub-heading font-bold text-secondary mb-2">SKILLS</h3>
-              <div className="h-fit flex flex-wrap justify-end gap-4 items-star size-full">
-                  {skills.map((x, i) => (
-                    <div className="size-28 p-4 gap-4 bg-secondary text-primary flex flex-col items-center justify-between duration-700 hover:-rotate-[60deg]">
-                      <h3 className="font-bold text-sm">{x.title}</h3>
-                      <p className="font-thin text-xs">{x.desc}</p>
-                    </div>
-                  ))}
+          {/* main text */}
+          <div className='md:col-start-4 md:col-span-3 md:row-span-2 text-end'>
+            <h3 className="text-lg font-bold text-secondary">HELLO!</h3>
+            <p className='text-sm font-thin text-secondary'>Nam libero tempore, cum soluta nobis est eligendi 
+               optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, 
+               omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut 
+               officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. 
+               Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut 
+               perferendis doloribus asperiores repellat.</p>
+          </div>
+          <div className='md:col-start-4 md:col-span-3 md:row-span-2 text-end'>
+            <h3 className="text-lg font-bold text-secondary">EXPERTISE</h3>
+            <p className='text-sm font-thin text-secondary'>
+              Frontend Design / <br/>
+              Frontend Development / <br/>
+              API Development / <br/>
+              UI/UX /<br/>
+              Graphic Design /<br/>
+            </p>
+          </div>
+          <div className='md:col-start-5 md:col-span-1 md:row-span-2 text-end'>
+            <h3 className="text-lg font-bold text-secondary">HOBBIES</h3>
+            <p className='text-sm font-thin text-secondary'>
+              Designing Posters / <br/> 
+              Watching Anime / <br/> 
+              Learning web animation / <br/> 
+              Gaming ~ Valorant/CS /<br/>
+            </p>
+          </div>
+          <div className='md:col-start-6 md:col-span-1 md:row-span-2 text-end'>
+            <h3 className="text-lg font-bold text-secondary">FAV PLACES</h3>
+            <p className='text-sm font-thin text-secondary'>
+              NYC! / <br/> 
+              Boston / <br/> 
+              Lahore /<br/> 
+            </p>
+          </div>
+        </section>
+        {/* TOP SKILLS */}
+        <section id="skills" className="h-fit w-screen p-[24px] pe-28 text-end">
+          <h3 className="text-lg font-bold text-secondary mb-4">TOP SKILLS</h3>
+          <div className="flex flex-wrap md:flex-row gap-4 items-center justify-end">
+            {skills.map((x, i) => (
+              <div className="size-32 flex flex-col items-center justify-center p-4 gap-4 bg-secondary text-primary duration-500 hover:-rotate-[60deg]">
+                <h3 className="font-bold text-sm">{x.title}</h3>
+                <p className="font-thin text-sm">{x.desc}</p>
               </div>
-            </div>
+            ))}
           </div>
-        </section>
-
-
-        {/* HOBBIES / INTERESTS */}
-        <section className="hobbies h-fit pe-28 p-24 md:p-4 md:pe-28 flex flex-col items-end md:flex-row md:justify-end md:items-start gap-24 md:gap-8 text-end">
-          <div className="size-fit gap-4 text-secondary flex flex-col items-end justify-between">
-            <h3 className="font-bold sub-heading">FAV PLACES</h3>
-            <p className="font-thin text-xs">New York City</p>
-            <p className="font-thin text-xs"></p>
-            <p className="font-thin text-xs"></p>
-            <p className="font-thin text-xs"></p>
-            <p className="font-thin text-xs"></p>
-          </div>
-          <div className="size-fit gap-4 text-secondary flex flex-col items-end justify-between">
-            <h3 className="font-bold sub-heading">FAV ANIMES</h3>
-            <p className="font-thin text-xs">Naruto</p>
-            <p className="font-thin text-xs">Jujutsu Kaisen</p>
-            <p className="font-thin text-xs">Hyouka</p>
-            <p className="font-thin text-xs">Demon Slayer</p>
-            <p className="font-thin text-xs">Attack on titan</p>
-          </div>
-          <div className="size-fit gap-4 text-secondary flex flex-col items-end justify-between">
-            <h3 className="font-bold sub-heading">HOBBIES</h3>
-            <p className="font-thin text-xs">Designing Movie Posters</p>
-            <p className="font-thin text-xs">Gaming ~ Valorant/CS2</p>
-            <p className="font-thin text-xs">Watching Animes</p>
-            <p className="font-thin text-xs">Drawing</p>
-          </div>
-        </section>
+        </section>  
     </div>
   )
 }
